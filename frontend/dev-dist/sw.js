4087bf8af72b35ba86fc20fef247c3b9246c7942
AKASH GURAV <guravakashparshuram10@gmail.com>
2026-07-06 13:11:23 +0530

    NAME LOGIN

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f0c192c2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.pk8m9gapou8"
+    "revision": "0.o5gbd31v6gg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
